437ae859a33733c8109c030bb405382382955f6a
Simon <40443443+texas0418@users.noreply.github.com>
2026-02-05 12:03:23 -0500

    Clean repo: remove node_modules, .DS_Store, .expo from tracking; update .gitignore

--- node_modules/@nkzw/create-context-hook/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { FunctionComponent, ReactNode } from 'react';
-
-declare function createContextHook<T>(contextInitializer: () => T, defaultValue?: T): [Context: FunctionComponent<{
-    children: ReactNode;
-}>, useHook: () => T];
-
-export { createContextHook as default };
